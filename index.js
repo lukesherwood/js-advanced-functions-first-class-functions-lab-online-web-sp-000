367fcb39fd7ff03a5522d27c39f148d9e636cede
Luke Sherwood <luke.sherwood55@gmail.com>
2020-06-10 09:42:55 +0000

    Done.

--- index.js
+++ index.js
@@ -15,3 +15,9 @@ const returnLastTwoDrivers = function(drivers) {
 
 const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
 
+const createFareMultiplier = function(integer){
+  return function (fare) {
+    return integer * fare
+  }
+}
+
