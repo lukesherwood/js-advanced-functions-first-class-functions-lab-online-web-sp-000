a05ac5bb95d2fcfc6a146dd61e83f1adf36c2b38
Luke Sherwood <luke.sherwood55@gmail.com>
2020-06-10 04:06:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers){
+  const array = []
+  array.push(drivers[0])
+  array.push(drivers[1])
+  return array
+}
+
+const returnLastTwoDrivers = function(drivers) {
+  const array = []
+  array.push(drivers[drivers.length-2])
+  array.push(drivers[drivers.length-1])
+  return array
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
